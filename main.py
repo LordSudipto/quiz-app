3f1ce08d3eaea979bca5bb50754218c1a4f8991e
Sudipto Majumder <sudiptomajumder07@gmail.com>
2022-04-26 17:24:25 +0530

    Project steps updated

---
+++ main.py
@@ -0,0 +1,30 @@
+# Import libraries.
+
+
+
+
+# Read data using Python from the MongoDB collection.
+
+
+
+# Display it for user in appropriate form using any Python code
+# (Use number bulleting for MCQ options for user to enter number corresponding to their answer,
+# mention 'one word only' for the remaining questions and accept only a one word answer)
+
+# AND
+
+# Allot 1 mark for each correct MCQ, 2 marks for each correct single word answer.
+# Store the marks for each question in a list/dictionary/set or any other data structure you deem fit.
+
+
+
+
+
+# After all questions are answered, display total marks achieved.
+
+
+
+# Use matplotlib or seaborn to create a graph highlighting performance in MCQ vs single word answers for
+# no. of questions answered correctly.
+# Use any type of graph you deem fit.
+
--- questions.py
+++ questions.py
@@ -1,3 +1,7 @@
+# Prepare 10 questions in JSON format in a JSON file. 5 MCQ, 5 single word answers.
+# Load JSON file into python program.
+# Read the JSON and insert questions into a MongoDB collection. Store this code in a separate Python script file
+
 # Script to add questions into mongoDB
 
 import pymongo
