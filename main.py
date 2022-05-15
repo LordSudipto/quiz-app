92420561618c9de6b82b75b668b91f05207068e2
Sudipto Majumder <sudiptomajumder07@gmail.com>
2022-05-15 19:50:50 +0530

    quiz complete, comments arranged

--- main.py
+++ main.py
@@ -11,22 +11,26 @@ conn_string = os.getenv('mongo_string')
 # Connect to MongoDB
 cluster = MongoClient(conn_string)
 db = cluster['Questions']
-collection1 = db['MCQ']                     # The collection in your database
-collection2 = db['One Word Answers']        # The collection in your database
+collection1 = db['MCQ']                     # The collection in your database.
+collection2 = db['One Word Answers']        # The collection in your database.
 collection3 = db['results']
 
+mcq_score = 0                               # Score for MCQ questions.
+owa_score = 0                               # Score for one word answer questions.
+
 # Read data using Python from the MongoDB collection
-#print(collection1.find_one({'_id': 1}))
 
 # Display questions for user in appropriate form using any Python code
-mcq_score = 0
-owa_score = 0
-print(type(collection2.find_one({'_id': 5})['Answer'][0]))
 
+# (Use numbered bullets for MCQ options for user to enter number corresponding to their answer,
+# mention 'one word only' for the remaining questions and accept only a one word answer)
+# AND
+# Allot 1 mark for each correct MCQ, 2 marks for each correct single word answer.
+# Store the marks for each question in a list/dictionary/set or any other data structure you deem fit.
 
 print('Welcome to the quiz app\n')
 print('------------------------\n')
-name = input('Enter your name')
+name = input('Enter your name\n')
 
 if input('press \'y\' to start\n'):
     mcq_questions = collection1.find({})
@@ -39,7 +43,6 @@ if input('press \'y\' to start\n'):
         if answer is mcq['Correct']:
             mcq_score+=1
 
-
     for owa in owa_questions:
         print(f"{owa['_id']}. {owa['Question']}")
         answer = input('Enter a one word answer: ').lower()
@@ -48,24 +51,13 @@ if input('press \'y\' to start\n'):
             owa_score+=1
         elif answer == str(owa['Answer']).lower():
             owa_score+=1
-print(mcq_score)
-print(owa_score)
-
-# (Use numbered bullets for MCQ options for user to enter number corresponding to their answer,
-# mention 'one word only' for the remaining questions and accept only a one word answer)
-
-# AND
-
-# Allot 1 mark for each correct MCQ, 2 marks for each correct single word answer.
-# Store the marks for each question in a list/dictionary/set or any other data structure you deem fit.
-
-
-
-
 
 # After all questions are answered, display total marks achieved.
-
-
+print('\nScoresheet')
+print('------------------------')
+print(f'Name: {name}')
+print(f'MCQ score: {mcq_score}')
+print(f'One word answer score: {owa_score}')
 
 # Use matplotlib or seaborn to create a graph highlighting performance in MCQ vs single word answers for
 # no. of questions answered correctly.
